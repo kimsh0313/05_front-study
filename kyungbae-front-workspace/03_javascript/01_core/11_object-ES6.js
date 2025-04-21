ae08495d44a5d1418c53df1886e64c787429c623
Kyungbae_Go <gokyungbae@gmail.com>
2025-04-21 12:28:42 +0900

    create : javascript Object

--- kyungbae-front-workspace/03_javascript/01_core/11_object-ES6.js
+++ kyungbae-front-workspace/03_javascript/01_core/11_object-ES6.js
@@ -3,7 +3,23 @@
   ES6에서는 객체 내의 메소드 정의시 function 키워드를 생략한 축약 표현을 사용할 수 있음
 */
 
+// ES6 -> 메소드 단축 x
+const dog1 = {
+  name: '뽀삐',
+  eat: function(food){
+    console.log(`${this.name}의 음식${food}`);
+  }
+};
 
+// 메소드 단축 구문
+const dog2 = {
+  name: '두부',
+  eat(food) {
+    console.log(`${this.name}의 음식: ${food}`);
+  }
+};
+
+dog2.eat('사료');
 
 /*
   ## 프로퍼티 값 단축 구문 ##
@@ -11,7 +27,18 @@
   단, 프로퍼티명(키)은 변수 이름으로 부여됨 
 */
 
+// 프로퍼티 값 단축 x
+let id = 'p-0001';
+let price = 300000;
+let product1 = {
+  id: id,
+  price: price
+};
 
+// 프로퍼티 값 단축
+let product2 = {id, price};
+console.log(product1);
+console.log(product2);
 
 /*
   ## 구조 분해 할당 ##
@@ -19,9 +46,38 @@
   2. 객체의 각 프로퍼티값을 개별 변수에 쉽게 할당할 수 있는 문법
   3. 단, 변수명을 객체의 프로퍼티명과 동일하게 맞춰야됨 
   4. 형식
-     {변수1, 변수2, ..} = 객체;
+      {변수1, 변수2, ..} = 객체;
 */
 
+const car = {
+  brand: 'hyundai',
+  model: 'i30',
+  year: 2019
+};
 
+// 구조 분해 할당 x
+// let brand = car.brand;
+// let model = car.model;
+// let year = car.year;
 
+// 구조 분해 할당
+// 1. 선언과 동시에 할당
+// let {brand, model, year} = car;
 
+// 2. 선언후 할당
+let brand, model, year;
+({brand, model, year} = car); // 소괄호로 묶어주어야 구조분해 할당 됨
+
+console.log(brand, model, year);
+
+// 변수명을 프로퍼티명과 다르게 하고 싶을때
+const qna = {
+  q: '한국의 수도는?',
+  a: '서울'
+};
+
+// let {question, answer} = qna; // 변수명이 프로퍼티명과 다르기 때문에 바로 대입 불가
+
+let {q: question, a:answer} = qna;
+
+console.log(question, answer);
